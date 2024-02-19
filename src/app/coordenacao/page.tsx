44c37529dff37d9efc0bc17593aeff632794f591
Lisandra Uehara <ueharalisandra@gmail.com>
2024-02-18 21:29:26 -0300

    adicionado prisma client

---
+++ src/app/coordenacao/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/container";
+
+export default function Coordenacao(){
+    return (
+       <Container>
+            <h1>Dashboard do coordenador</h1>
+        </Container>
+    )
+}
